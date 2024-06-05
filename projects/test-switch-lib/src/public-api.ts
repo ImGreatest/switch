5db29634784bda222bf5508b45e5996fa1c80f75
ImGreatest <cmayll@mail.ru>
2024-06-05 16:50:58 +0300

    pon2

--- projects/test-switch-lib/src/public-api.ts
+++ projects/test-switch-lib/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of test-switch-lib
  */
 
-export * from './lib/test-switch-lib.service';
 export * from './lib/test-switch-lib.component';
+export * from './lib/test-switch-lib.module';
+export * from './lib/test-switch-lib.options';
